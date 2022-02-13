33c7b07c0430c5a04a50444b7f1fc32742b53cbd
VERNYGAMER <emanuel.cardenas@alumnos.upm.es>
2022-02-13 20:33:26 +0100

    Segunda subrutina

--- modulos.f95
+++ modulos.f95
@@ -20,5 +20,26 @@ module ordenar
             write(unit=10, fmt='(8(I4))', iostat=ierr) X
         end do
     end subroutine ord_intercambio
+
+!_________________________________________________________________________________________________
+    subroutine ord_seleccion(X,n) 
+        integer :: i, j, aux, min
+        integer :: X(n)
+
+        X=(/2,0,6,5,3,1,8,4/)
+
+        do i=1,n-1       !Recorremos todas las posiciones
+            min=i        !Tomamos como minimo la primera posicion
+            do j=i+1,n   !Buscamos el minimo a la derecha de i
+                if (X(min)>X(j)) then 
+                    min=j
+                end if   !Se cambian las posiciones para que el minimo se quede a la izquierda
+            end do
+            aux=X(i)
+            X(i)=X(min)  
+            X(min)=aux 
+            write(unit=10, fmt='(8(I4))', iostat=ierr) X
+        end do
+    end subroutine ord_seleccion
     
 end module
